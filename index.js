06718bb7a41d8dc799ed3ce34f8689fc5cb4f803
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-08 18:20:05 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+ var katzDeliLine = [];
+function takeAnumber(currentline, name) {
+  
+   currentline.push(name);
+   return "Welcome, " + name + ". You are number" + currentline.length + " in line.";
+   
+}
+
+function 
